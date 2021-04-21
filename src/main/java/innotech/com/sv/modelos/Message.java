fe5c30ef5f7facaaa59f63c5cd15069668bc47e7
sisa <cipotex@hotmail.com>
2021-04-20 23:05:09 -0600

    se actualizan modelos de datos

---
+++ src/main/java/innotech/com/sv/modelos/Message.java
@@ -0,0 +1,90 @@
+package innotech.com.sv.modelos;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="messages")
+public class Message implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date fecha_creacion;
+	
+	@PrePersist
+	public void preinsert() {
+		this.fecha_creacion = new Date();
+	}
+	
+	@NotBlank(message = "No se puede dejar vacio destinatario")
+	private String para1;
+	
+	private String para2;
+	
+	@NotBlank(message = "No se puede dejar vacio el mensaje")
+	private String mensaje;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Date getFecha_creacion() {
+		return fecha_creacion;
+	}
+
+	public void setFecha_creacion(Date fecha_creacion) {
+		this.fecha_creacion = fecha_creacion;
+	}
+
+	public String getPara1() {
+		return para1;
+	}
+
+	public void setPara1(String para1) {
+		this.para1 = para1;
+	}
+
+	public String getPara2() {
+		return para2;
+	}
+
+	public void setPara2(String para2) {
+		this.para2 = para2;
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	
+	
+	
+}
---
+++ src/main/java/innotech/com/sv/modelos/SendMessage.java
@@ -0,0 +1,79 @@
+package innotech.com.sv.modelos;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="SendMessages")
+public class SendMessage implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@ManyToOne
+	private Message message;
+	
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+	private Date date_time_Send;
+	
+	@NotBlank(message = "No se puede dejar vacio la confirmacion Twilio")
+	private String TwilioConfirmation;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Message getMessage() {
+		return message;
+	}
+
+	public void setMessage(Message message) {
+		this.message = message;
+	}
+
+	public Date getDate_time_Send() {
+		return date_time_Send;
+	}
+
+	public void setDate_time_Send(Date date_time_Send) {
+		this.date_time_Send = date_time_Send;
+	}
+
+	public String getTwilioConfirmation() {
+		return TwilioConfirmation;
+	}
+
+	public void setTwilioConfirmation(String twilioConfirmation) {
+		TwilioConfirmation = twilioConfirmation;
+	}
+	
+	
+	
+	
+	
+}
