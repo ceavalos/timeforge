05c7027bcfe99db48fe1451145037d3e8b1908cf
sisa <cipotex@hotmail.com>
2021-04-23 23:52:23 -0600

    actualizacion

--- src/main/java/innotech/com/sv/controllers/MensajesController.java
+++ src/main/java/innotech/com/sv/controllers/MensajesController.java
@@ -8,22 +8,18 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import innotech.com.sv.modelos.Message;
 
 
 
 @Controller
 @SessionAttributes("messagee")
-//@RequestMapping("/mensaje")
+@RequestMapping("/mensaje")
 public class MensajesController {
 
-	@RequestMapping("/")
+	@RequestMapping("/envio")
 	public String inicio(Model modelo,  HttpServletRequest request) {
 		
 		Message message = new Message();
@@ -36,12 +32,14 @@ public class MensajesController {
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
 	public String forma(@Valid Message message, BindingResult result, Model model,
 			            SessionStatus status) {
-		
+		//
+		System.out.println("En controlador forma =" + result.hasErrors());
+		//
 		if (result.hasErrors()) {
 			System.out.println("con errores al momento de enviar mensaje");
-			return "mensaje";
+			return "/";
 		}
 		
-		return "redirect:mensaje";
+		return "redirect:/";
 	}
 }
